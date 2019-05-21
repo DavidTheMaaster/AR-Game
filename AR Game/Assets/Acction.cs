4736c517d0980b5afea249067dd2336c390c0aeb
DavidLozanoSánchez <davidthemaaster@gmail.com>
2019-05-21 15:18:33 +0200

    Added Gun Animations

--- AR Game/Assets/Acction.cs
+++ AR Game/Assets/Acction.cs
@@ -5,32 +5,49 @@ using UnityEngine;
 public class Acction : MonoBehaviour
 {
     public GameObject gun, ammo, shield;
+    public Animator gun_animation;
     private bool gun_seen, ammo_seen, shield_seen;
+    private int counter = 0;
+
+    void Start()
+    {
+        gun_animation = gun.transform.GetChild(0).GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(!gun_seen && !ammo_seen)
+        if (counter == 0)
         {
-            Protect();
+            if (!gun_seen && !ammo_seen && shield_seen)
+            {
+                Protect();
+            }
+
+            if (!shield_seen && !ammo_seen && gun_seen)
+            {
+                Shoot();
+            }
+
+            if (!gun_seen && !shield_seen && ammo_seen)
+            {
+                Recharge();
+            }
+
+            TestSeen();
         }
 
-        if (!shield_seen && !ammo_seen)
-        {
-            Shoot();
-        }
-
-        if (!gun_seen && !shield_seen)
-        {
-            Recharge();
-        }
-
-        TestSeen();
+        Timer();
     }
 
     void Shoot()
     {
         Debug.Log("SHOOT");
+
+        if(gun_animation != null)
+        {
+            gun_animation.SetBool("Shoot", true);
+        }
     }
 
     void Recharge()
@@ -48,5 +65,18 @@ public class Acction : MonoBehaviour
         gun_seen = gun.GetComponent<DefaultTrackableEventHandler>().seen;
         ammo_seen = ammo.GetComponent<DefaultTrackableEventHandler>().seen;
         shield_seen = shield.GetComponent<DefaultTrackableEventHandler>().seen;
+
+        if(gun_seen && ammo_seen && shield_seen)
+        {
+            if (gun_animation != null)
+            {
+                gun_animation.SetBool("Shoot", false);
+            }
+        }
+    }
+
+    void Timer()
+    {
+
     }
 }
